95ba92c5338ced4f05c624118e6a4f91a68f1ea7
Tamiris Shigaki <shigakiendo@gmail.com>
2022-03-22 18:43:28 -0300

    requsiito 3: ok

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,4 +1,57 @@
-describe('', () => {
-  it('', () => {
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+const mockFavorites = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+  },
+  {
+    id: 10,
+    name: 'Caterpie',
+    type: 'Bug',
+    averageWeight: {
+      value: '2.9',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/8/83/Spr_5b_010.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)',
+  },
+];
+
+describe('Teste do componente <FavoritePokemons.js />', () => {
+  it('Verifica se é exibido na tela a mensagem "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const msg = screen.getByText(/No favorite pokemon found/i);
+    expect(msg).toBeDefined();
+  });
+
+  it('Verifica se são exibidos todos os cards de pokemons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ mockFavorites } />);
+
+    const number = 3;
+    const favorites = screen.getAllByTestId('pokemon-name');
+    expect(favorites).toHaveLength(number);
   });
 });
