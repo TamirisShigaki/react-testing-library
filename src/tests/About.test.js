4776d2a064b9f3fe879e8ad3cf05f686e23fa915
Tamiris Shigaki <shigakiendo@gmail.com>
2022-03-22 16:18:26 -0300

    requisito 2: ok

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,4 +1,31 @@
-describe('', () => {
-  it('', () => {
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Testa se a página contém as informações sobre a Pokédex', () => {
+  it('Verifica se a página contém um heading h2 com o texto "About Pokédex"', () => {
+    renderWithRouter(<About />);
+
+    const title = screen.getByRole('heading',
+      { name: 'About Pokédex' });
+    expect(title).toBeDefined();
+  });
+
+  it('Verifica se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const paragraph1 = screen.getByText(/containing all Pokémons/i);
+    expect(paragraph1).toBeDefined();
+
+    const paragraph2 = screen.getByText(/filter Pokémons/i);
+    expect(paragraph2).toBeDefined();
+  });
+
+  it('Verifica se a página contém imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const image = screen.getByRole('img');
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,21 +5,21 @@ import App from '../App';
 
 describe('Testa se o topo da aplicação contém um conjunto fixo de links de navegação.',
   () => {
-    it('Verifica se o primeiro link deve possuir o texto Home', () => {
+    it('Verifica se o primeiro link deve possuir o texto "Home"', () => {
       renderWithRouter(<App />);
 
       const home = screen.getByText('Home');
       expect(home).toBeDefined();
     });
 
-    it('Verifica se o segundo link deve possuir o texto About', () => {
+    it('Verifica se o segundo link deve possuir o texto "About"', () => {
       renderWithRouter(<App />);
 
       const about = screen.getByText('About');
       expect(about).toBeDefined();
     });
 
-    it('Verifica se o terceiro link deve possuir o texto Favorite Pokémons', () => {
+    it('Verifica se o terceiro link deve possuir o texto "Favorite Pokémons"', () => {
       renderWithRouter(<App />);
 
       const favoritePokemons = screen.getByText('Favorite Pokémons');
