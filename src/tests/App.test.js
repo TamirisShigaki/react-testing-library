89c6d30e64d5e9cbbf391f5708988831afb34e69
Tamiris Shigaki <shigakiendo@gmail.com>
2022-03-22 17:12:09 -0300

    requisito 1: faltou parte do test. ok

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
@@ -25,4 +26,42 @@ describe('Testa se o topo da aplicação contém um conjunto fixo de links de na
       const favoritePokemons = screen.getByText('Favorite Pokémons');
       expect(favoritePokemons).toBeDefined();
     });
+
+    it('verifica se ao clicar em "Home", é redirecionado pra Home ', () => {
+      const { history } = renderWithRouter(<App />);
+
+      const linkHome = screen.getByRole('link', { name: /home/i });
+      expect(linkHome).toBeInTheDocument();
+      userEvent.click(linkHome);
+      expect(history.location.pathname).toBe('/');
+    });
+
+    it('verifica se ao clicar em "About", é redirecionado pra About ', () => {
+      const { history } = renderWithRouter(<App />);
+
+      const linkAbout = screen.getByRole('link', { name: /about/i });
+      expect(linkAbout).toBeInTheDocument();
+      userEvent.click(linkAbout);
+      expect(history.location.pathname).toBe('/about');
+    });
+
+    it('verifica se ao clicar em "Favorite Pokémons", é redirecionado pra pagina', () => {
+      const { history } = renderWithRouter(<App />);
+
+      const linkFavorite = screen.getByRole('link', { name: /favorite/i });
+      expect(linkFavorite).toBeInTheDocument();
+      userEvent.click(linkFavorite);
+      expect(history.location.pathname).toBe('/favorites');
+    });
+
+    it('Verifica um caminho não existente e redireciona para Not Found',
+      () => {
+        const { history } = renderWithRouter(<App />);
+
+        history.push('/xablau');
+
+        const notFoundTitle = screen.getByRole('heading',
+          { name: /Page requested not found/i });
+        expect(notFoundTitle).toBeInTheDocument();
+      });
   });
